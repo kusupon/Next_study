06bd82154c4753d78e10b581ff83650e6f4571c0
kusupon <kusupon0628@gmail.com>
2022-05-24 10:18:27 +0900

    useStateとuseEffectのお勉強

---
+++ pages/switch-demo.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const SwitchDemo = () => {
+    const [pushed, setPushed] = useState(false);
+    useEffect(() => {
+        document.title = `現在の状態: ${pushed ? "ON" : "OFF"}`
+    }, [pushed]);
+    return (
+        <main style={{ textAlign: "center" }}>
+            <p>押してね</p>
+            <button 
+            onClick={() => {setPushed(!pushed)}}
+            style={{ background: pushed ? "pink" : "skyblue" }}
+            >
+            {pushed ? "ON" : "OFF"}
+            </button>
+        </main>
+    );
+};
+
+export default SwitchDemo;
